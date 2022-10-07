53192a00830393ebf167faaed11a78477b75c5d1
Alejandro Carmona Ramirez <alejocram@gmail.com>
2022-10-06 22:52:53 -0700

    Add TableView to View Controller and create UITableViewDataSource with sections and rows from mock data

--- HelloTableView/ViewController.swift
+++ HelloTableView/ViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var sections = ["Animals", "Fruits", "Cars"]
+    var rows: [[String]] = [
+        ["Dolphin", "Cat", "Dog", "Ginnea Pig"],
+        ["Apple", "Pears", "Plum"],
+        ["Audi", "Tesla", "BMW"]
+    ]
+    
+    @IBOutlet var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.dataSource = self
     }
-
-
 }
 
+extension ViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return self.sections.count
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return self.sections[section]
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return rows[section].count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = rows[indexPath.section][indexPath.row]
+        return cell
+    }
+}
